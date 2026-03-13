938e452fd91db2d9f65e1b4d8b20995cf7d2c7da
Sahatsawat-Wattana <sahatsawat.wattanapraditchai@gmail.com>
2026-03-13 15:26:03 +0700

    committed friday

---
+++ mongo-db/query.mongodb.js
@@ -0,0 +1,11 @@
+use('chrome_burger');
+
+//db.staff.aggregate();
+
+//db.menu_items.aggregate([{$match:{category:"Burgers"}}]);
+
+//db.menu_items.aggregate([{$sort:{price:-1}}]);
+
+//db.menu_items.aggregate([{$sort:{price:-1}},{$limit:3}]);
+
+//
